a164295da03913e1da6ed105f46cd6ccec1d3e44
ytlintw <tw6873@gmail.com>
2018-04-24 21:56:26 +0200

    initial commit

---
+++ ivet/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Choice, Question
+
+# Register your models here.
+#we need to tell the admin that Question objects have an admin interface.
+
+from .models import Question
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
---
+++ ivet/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+# add the name of app
+app_name="ivet"
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
